f0289d7961b4691734ab5f665396c4d9821c8770
Ritesh Yadav <riteshyadav070802@gmail.com>
2023-01-12 17:20:23 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/taazakhabar/healthfragment.java
@@ -0,0 +1,59 @@
+package com.example.taazakhabar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class healthfragment extends Fragment {
+    String api= "a8839e7c8c2441d181e8d145d8e93844";
+    ArrayList<ModelClass> modelClassArrayList;
+    Adapter adapter;
+    String country = "in";
+    private RecyclerView recyclerViewofhealth;
+    private String category = "health";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.healthfragment,null);
+
+        recyclerViewofhealth = v.findViewById(R.id.recyclerviewofhealth);
+        modelClassArrayList = new ArrayList<>();
+        recyclerViewofhealth.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new Adapter(getContext(),modelClassArrayList);
+        recyclerViewofhealth.setAdapter(adapter);
+        findNews();
+        return v;
+    }
+
+    private void findNews() {
+
+        ApiUtilites.getApiInterface().getCategoryNews(country,category,100,api).enqueue(new Callback<mainNews>() {
+            @Override
+            public void onResponse(Call<mainNews> call, Response<mainNews> response) {
+                if(response.isSuccessful()){
+                    modelClassArrayList.addAll(response.body().getArticles());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<mainNews> call, Throwable t) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/taazakhabar/webview.java
@@ -0,0 +1,30 @@
+package com.example.taazakhabar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toolbar;
+
+public class webview extends AppCompatActivity {
+    Toolbar toolbar;
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+        toolbar = findViewById(R.id.toolbar);
+        webView = findViewById(R.id.webview);
+        setSupportActionBar(toolbar);
+
+        Intent intent=getIntent();
+        String url = intent.getStringExtra("url");
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+}
